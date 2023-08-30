aaded1fd8091fc2b544ff7c26f60d79d60e35c3b
Qwazertox <qwazertyx84@gmail.com>
2023-08-30 03:05:40 +0200

    creation of module and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TestModule } from './modules/test/test.module';
+import { TestController } from './modules/test/test.controller';
 
 @Module({
-  imports: [],
+  imports : [TestModule],
   controllers: [AppController],
   providers: [AppService],
 })
